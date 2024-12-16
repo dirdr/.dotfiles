ea9974553a96163b341f547160a8d27813113775
dirdr <adrien.pelfresne@gmail.com>
2024-12-16 10:13:51 +0100

    feat(nvim): Add custom autocmd and keymaps

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Add any additional autocmds herevim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("FormatOptions", {}),
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "r", "o" })
+  end,
+})
